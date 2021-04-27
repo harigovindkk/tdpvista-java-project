f77e3a3cc3f16d271f6dde05f87cbe3ccdb4679a
Hari Govind K K <45679986+harigovindkk@users.noreply.github.com>
2021-04-27 23:20:42 +0530

    Language Detection and Translation

---
+++ LanguageDetection.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.language.LanguageIdentifier;
+
+import org.xml.sax.SAXException;
+
+public class LanguageDetection {
+
+   public static void main(String args[])throws IOException, SAXException, TikaException {
+
+      LanguageIdentifier identifier = new LanguageIdentifier("this is english ");
+      String language = identifier.getLanguage();
+      System.out.println("Language of the given content is : " + language);
+   }
+}
